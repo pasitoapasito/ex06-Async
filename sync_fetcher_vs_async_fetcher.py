d5bac147b86b70ef8f98b7525d07f81b349fe1cb
DGK <pasito.dev@hotseller.co.kr>
2022-12-08 13:36:41 +0900

    modify: 동기/비동기 fetcher 함수 비교

--- sync_fetcher_vs_async_fetcher.py
+++ sync_fetcher_vs_async_fetcher.py
@@ -31,7 +31,7 @@ async def async_func():
     urls = ["https://naver.com", "https://google.com", "https://instagram.com"] * 10
 
     """
-    aiohttp: 비동기적 요청 라이브러리 활용
+    aiohttp: 비동기적 요청 라이브러리 활용(비동기적 요청을 동시성으로 처리)
     """
     async with aiohttp.ClientSession() as session:
         result = await asyncio.gather(*[async_fetcher(session, url) for url in urls])
