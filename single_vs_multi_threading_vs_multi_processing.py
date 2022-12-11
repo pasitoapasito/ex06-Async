061b1ee64a77b4717823c5284190efe0a40686ac
DGK <pasito.dev@hotseller.co.kr>
2022-12-11 19:58:47 +0900

    modify: 싱글/멀티 쓰레드/멀티 프로세스 cpu bound 함수 비교

--- single_vs_multi_threading_vs_multi_processing.py
+++ single_vs_multi_threading_vs_multi_processing.py
@@ -22,17 +22,26 @@ def cpu_bound_func(num):
     return total
 
 
+"""
+싱글 쓰레드
+"""
 def single_threading_func():
     results = [cpu_bound_func(num) for num in nums]
     print(results)
 
 
+"""
+멀티 쓰레드(thread: 10개)
+"""
 def multi_threading_func():
     executor = ThreadPoolExecutor(max_workers=10)
     results = list(executor.map(cpu_bound_func, nums))
     print(results)
 
 
+"""
+멀티 프로세스(process: 10개)
+"""
 def multi_processing_func():
     executor = ProcessPoolExecutor(max_workers=10)
     results = list(executor.map(cpu_bound_func, nums))
@@ -40,16 +49,25 @@ def multi_processing_func():
 
 
 if __name__ == "__main__":
+    """
+    싱글 쓰레드 함수 호출
+    """
     single_start = time.time()
     single_threading_func()
     single_end = time.time()
     print(single_end - single_start)  # 0.0001
 
+    """
+    멀티 쓰레드 함수 호출
+    """
     multi_threading_start = time.time()
     multi_threading_func()
     multi_threading_end = time.time()
     print(multi_threading_end - multi_threading_start)    # 0.0003
 
+    """
+    멀티 프로세스 함수 호출
+    """
     multi_processing_start = time.time()
     multi_processing_func()
     multi_processing_end = time.time()
